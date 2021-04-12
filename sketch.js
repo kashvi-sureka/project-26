67bfd4d48771e0655f8da598b51eeaa0a2a5c238
kashvi-sureka <75981003+kashvi-sureka@users.noreply.github.com>
2021-04-12 17:26:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+steps to make cheese grilled sandwich
+
+1. Add 1 to 2 teaspoons butter to a hot pan and melt it. Spread it with a spatula when it melts. Place the bread slices over the melted butter. 
+You can also smear the softened butter over the slices. If your butter isn’t soft, then you can also add it to the hot pan. You can use as much butter as you like.
+
+2. Toast the bread slices until golden on one side of both the slices.
+
+3. Flip one of them and place a cheese slice or grated cheese. Sprinkle the flavorings you prefer like pepper, red chili flakes and oregano. Place the golden side over the cheese.
+
+Placing the cheese over the toasted hot bread helps the cheese to melt fast.
+
+4. Add some more butter to the pan. Flip and toast on both the sides on a very low heat until the cheese melts off.
+
+5. Take care not to burn. Halve the grilled sandwich and serve hot or warm.
+
